8ff3fa1fabc3c1a492c4c30b20bd709f5b385ed9
Aaron Boakye <cheminko@hotmail.com>
2026-02-19 01:37:29 +0000

    added CRUD and routes for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,52 @@ class OrdersController < ApplicationController
   rescue_from AASM::InvalidTransition, with: :handle_invalid_transition
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
-  def ship
+  def create
+    order = Order.new(order_params)
+    if order.save
+      render json: order, status: :created
+    else
+      render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
     order = find_order
-    Orders::TransitionService.call(order: order, event: :ship)
     render json: order
   end
 
+  def index
+    orders = Order.all
+    render json: orders
+  end
+
+  def confirm
+    transition(:confirm)
+  end
+
+  def ship
+    transition(:ship)
+  end
+
+  def deliver
+    transition(:deliver)
+  end
+
+  def cancel
+    transition(:cancel)
+  end
+
   private
+
+  def transition(event)   #maintainable
+    order = find_order
+    Orders::TransitionService.call(order: order, event: event)
+    render json: order
+  end
+
+  def order_params
+    params.require(:order).permit(:total)
+  end
 
   def find_order
     Order.find(params[:id])
